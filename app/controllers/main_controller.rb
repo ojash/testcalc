7b547a29e536e42805a89f9e4045ac12feba657e
spoudyal <subashpoudyal@lftechnology.com>
2013-10-30 14:40:34 +0545

    evaluate postfix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,18 +2,56 @@ class MainController < ApplicationController
 	def index
 	end
 	def calculate
-		#binding.pry
+		# binding.pry
 		if equation_validate(params[:q])
 			p_eqn = to_postfix(params[:q])
-			redirect_to main_index_path,:notice=> p_eqn.join 
+			result = evaluate_postfix(p_eqn)
+			redirect_to main_index_path,:notice=> p_eqn.join + " = " + result.to_s
 		else
 			redirect_to main_index_path,:notice=>"invalid character"
 		end
 	end
 
 	private
+	def evaluate_postfix(eqn)
+		@p_values = Array.new
+		eqn.each do |eqn_character|
+			if (eqn_character.match(/[\d]/))
+				numeric_val = eqn_character
+				push_values(numeric_val)
+
+			else 
+				operator_val = eqn_character
+				pop_values(operator_val)
+			end
+			# binding.pry
+		end
+		@result
+	end
+	def push_values(numeric_val)
+		@p_values << numeric_val
+		# binding.pry
+	end
+
+	def pop_values(operator_val)
+		# push_values.p_values
+		o_value = operator_val
+		# binding.pry
+		b = @p_values.pop.to_f
+		a = @p_values.pop.to_f
+		case o_value
+			when '+'; @result = a +  b
+			when '-'; @result = a -  b
+			when '*'; @result = a *  b
+			when '/'; @result = a /  b
+		end
+		@p_values << @result
+		# binding.pry
+	end
+
+
 	def equation_validate(eqn)
-		eqn.match(/[^\d\/\-\/*()+%]/) ? false :true
+		eqn.match(/[^\d\/\-\/*+]/) ? false :true
 	end
 	def to_postfix(eqn)
 		eqn = eqn.split("")
